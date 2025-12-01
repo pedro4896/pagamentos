33ef77815341dcd68d5774ba149f190afa658cc0
Pedro Victor da Silva Pereira <pedrovictordasilvapereira4896@gmail.com>
2025-12-01 20:23:31 -0300

    feat: implementar classes concretas de MPay e PPay

---
+++ src/adapter/PPayImpl.java
@@ -0,0 +1,30 @@
+// PPayImpl.java
+public class PPayImpl implements PPay {
+    private String custCardNo;
+    private String cardOwnerName;
+    private String cardExpMonthDate;
+    private Integer cvvNo;
+    private Double totalAmount;
+
+    @Override
+    public String getCustCardNo() { return custCardNo; }
+    @Override
+    public String getCardOwnerName() { return cardOwnerName; }
+    @Override
+    public String getCardExpMonthDate() { return cardExpMonthDate; }
+    @Override
+    public Integer getCVVNo() { return cvvNo; }
+    @Override
+    public Double getTotalAmount() { return totalAmount; }
+
+    @Override
+    public void setCustCardNo(String custCardNo) { this.custCardNo = custCardNo; }
+    @Override
+    public void setCardOwnerName(String cardOwnerName) { this.cardOwnerName = cardOwnerName; }
+    @Override
+    public void setCardExpMonthDate(String cardExpMonthDate) { this.cardExpMonthDate = cardExpMonthDate; }
+    @Override
+    public void setCVVNo(Integer cvvNo) { this.cvvNo = cvvNo; }
+    @Override
+    public void setTotalAmount(Double totalAmount) { this.totalAmount = totalAmount; }
+}
